6de29988dfcbec276f379382b655dac5a8276443
maheshabeykoon <maheshpabeykoon@gmail.com>
2022-07-24 23:06:38 +0530

    Updates

--- src/main/java/com/mahezh/login/config/SecurityConfiguration.java
+++ src/main/java/com/mahezh/login/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.mahezh.login.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,10 +17,10 @@ import com.mahezh.login.service.UserService;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+	@Lazy
 	@Autowired
 	private UserService userService;
-	
+
 	@Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
